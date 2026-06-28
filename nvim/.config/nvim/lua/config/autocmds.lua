2de69ff83205b63e37ef0871db3b05959c6c75fe
Dagim G. Astatkie <workflow.jd@gmail.com>
2026-06-28 21:52:03 +0300

    feat: add split navigation keymaps and options
    Map C-p to fzf-lua with ctrl-x for vsplit, Tab to toggle windows. Set splitright option. Add autocmd to close split when buffer is deleted (empty buffer and duplicate detection).
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,30 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("close_on_bdelete", { clear = true }),
+  desc = "Close split when its buffer is deleted",
+  callback = function()
+    vim.schedule(function()
+      if vim.fn.winnr("$") <= 1 then
+        return
+      end
+
+      local buf = vim.api.nvim_get_current_buf()
+      local win = vim.api.nvim_get_current_win()
+
+      if vim.fn.bufname(buf) == "" and not vim.bo[buf].modified then
+        pcall(vim.api.nvim_win_close, win, true)
+        return
+      end
+
+      for _, win_id in ipairs(vim.api.nvim_list_wins()) do
+        if win_id ~= win and vim.api.nvim_win_get_buf(win_id) == buf then
+          pcall(vim.api.nvim_win_close, win, true)
+          return
+        end
+      end
+    end)
+  end,
+})
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down & recenter" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up & recenter" })
 
-vim.api.nvim_set_keymap(
-  "n",
-  "<M-CR>",
-  '<cmd>lua require("fzf-lua").files({ actions = { ["default"] = require("fzf-lua.actions").file_split } })<CR>',
-  { noremap = true, silent = true }
-)
+vim.keymap.set("n", "<C-p>", function()
+  require("fzf-lua").files({
+    actions = {
+      default = require("fzf-lua.actions").file_edit,
+      ["ctrl-x"] = require("fzf-lua.actions").file_vsplit,
+    },
+  })
+end, { desc = "Find files (ctrl-x = vsplit)" })
+
+vim.keymap.set("n", "<Tab>", "<C-w>p", { desc = "Toggle between splits" })
+
+vim.keymap.set("n", "<M-CR>", function()
+  require("fzf-lua").files({
+    actions = {
+      default = require("fzf-lua.actions").file_split,
+    },
+  })
+end, { desc = "Find files (split)" })
