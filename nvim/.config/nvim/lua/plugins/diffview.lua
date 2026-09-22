65b148ee63800de21a08bfff9d388c6b4ef959f2
Dagim G. Astatkie <workflow.jd@gmail.com>
2026-09-22 10:57:38 +0300

    feat(nvim): add diffview plugin and refresh lockfile

---
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+  },
+}
